84a71b4b09bb03fde2061493491d75eeef51f1d9
Jan Reil Romero Daguil <124754196+jmgc95@users.noreply.github.com>
2025-09-12 23:52:50 +0800

    Update ebooks.js

--- ebooks.js
+++ ebooks.js
@@ -69,20 +69,20 @@
         {
           "title": "Visual C# Step by Step (Eigth Edition)",
           "subtitle": "Beginner to Advanced",
-          "pdfFileName": "https://tg-cloud-dl.noyes.in/download/2d6bc9bd-167e-4804-8c77-8b82fd7a8645",
-          "iconUrl": "https://tg-cloud-file-small-file.ajz.workers.dev/photos/file_383518.jpg?file_name=aqadb8cxg96nivz.jpg&expire=1757680926&signature=nZcaZ8QsWvV3DAXXrl7CCzltQAMMcSw50INMdl9qdmI%3D"
+          "pdfFileName": "http://jmgctv-stream.online/John-Sharp-Microsoft-Visual-CSharp.pdf",
+          "iconUrl": "http://jmgctv-stream.online/csharp.jpg"
         },
         {
           "title": "Java Programming",
           "subtitle": "From Problem Analysis to Program Design",
           "pdfFileName": "https://tg-cloud-file-small-file.ajz.workers.dev/documents/file_383515.pdf?file_name=course-technology-java-programming-from-problem-analysis-to-program.pdf&expire=1757681333&signature=UDOuwBYVc3LRG2ph7BT54mIYX79UIAKOWZ0UGoMhH6M%3D",
-          "iconUrl": "https://tg-cloud-file-small-file.ajz.workers.dev/photos/file_383516.jpg?file_name=aqadbscxg96nivz.jpg&expire=1757680910&signature=608mamra5R4u8RK8jDTR6KSmpomlAYBvrzKQBhDDe%2BQ%3D"
+          "iconUrl": "http://jmgctv-stream.online/java.jpg"
         },
         {
           "title": "C HOW TO PROGRAM (Eight & Global Edition)",
           "subtitle": "With introduction to C++",
-          "pdfFileName": "https://tg-cloud-file-small-file.ajz.workers.dev/documents/file_383526.pdf?file_name=c-how-to-program-with-an-introduction-to-c-global-edition-8th-edition.pdf&expire=1757681358&signature=ZnBJgrQtQSYxxPfROLP5oaZXXLRnI40qKGnK5PUTYpQ%3D",
-          "iconUrl": "https://tg-cloud-file-small-file.ajz.workers.dev/photos/file_383528.jpg?file_name=aqadkmcxg96nivz.jpg&expire=1757681236&signature=%2BHX%2FOiW%2F15rfzx60u8zuzor3c3fj6qVHy9Msv0TBPiI%3D"
+          "pdfFileName": "http://jmgctv-stream.online/",
+          "iconUrl": "http://jmgctv-stream.online/java.jpg"
         }
       ]
     }
