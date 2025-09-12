b01c07742601542d3ddbe0e8d7ba674786a1ee3f
Jan Reil Romero Daguil <124754196+jmgc95@users.noreply.github.com>
2025-09-12 19:37:05 +0800

    Update ebooks.js

--- ebooks.js
+++ ebooks.js
@@ -67,7 +67,7 @@
       "name": "Computer Programming",
       "books": [
         {
-          "title": "Visual C# Step by Step (Eighth Edition)",
+          "title": "Visual C# Step by Step (Eigth Edition)",
           "subtitle": "Beginner to Advanced",
           "pdfFileName": "https://tg-cloud-dl.noyes.in/download/21cf11c8-1857-496c-b6de-2db644dd430b",
           "iconUrl": "https://tg-cloud-file-small-file.ajz.workers.dev/photos/file_383518.jpg?file_name=aqadb8cxg96nivz.jpg&expire=1757674788&signature=usJ7i5CxZ%2BD%2FelQP0%2Brh2V4l9QztZsHwiIgFbytg%2BCw%3D"
@@ -77,6 +77,13 @@
           "subtitle": "From Problem Analysis to Program Design",
           "pdfFileName": "https://tg-cloud-file-small-file.ajz.workers.dev/documents/file_383522.pdf?file_name=course-technology-java-programming-from-problem-analysis-to-program.pdf&expire=1757675923&signature=ZAN0%2FMjokJEdUo%2B0Hjny209GDh%2F%2FAdl25FeHgzTNpG8%3D",
           "iconUrl": "https://tg-cloud-file-small-file.ajz.workers.dev/photos/file_383516.jpg?file_name=aqadbscxg96nivz.jpg&expire=1757674757&signature=ycP14OaBNSukISbvjZ0PfhTGkNOiFUBIqrCtx4YiT4Y%3D"
+        },
+        {
+          "title": "C HOW TO PROGRAM )(Eight & Gobal Edition",
+          "subtitle": "With introduction to C++",
+          "pdfFileName": "https://tg-cloud-file-small-file.ajz.workers.dev/documents/file_383526.pdf?file_name=c-how-to-program-with-an-introduction-to-c-global-edition-8th-edition.pdf&expire=1757677327&signature=Vc8v6CnCR7bN57lt%2FDqroSlDgO%2FmqTX%2Fy5KDjt4G%2FOo%3D",
+          "iconUrl": "https://tg-cloud-file-small-file.ajz.workers.dev/photos/file_383516.jpg?file_name=aqadbscxg96nivz.jpg&expire=1757674757&signature=ycP14OaBNSukISbvjZ0PfhTGkNOiFUBIqrCtx4YiT4Y%3D"
+        },
         }
       ]
     }
